bc0c27270c1176e2eff0cd3d2e4da4cd288fe8a2
Adrian-Cealic <adrian.cealic@gmail.com>
2024-07-11 22:35:24 +0300

    modified

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+// server.js
+var jsonServer = require('json-server')
+var server = jsonServer.create()
+var router = jsonServer.router('db.json')
+var middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(router);
-server.listen(port);
+server.use(middlewares)
+server.use(router)
+server.listen(3000, function () {
+    console.log('JSON Server is running')
+})
